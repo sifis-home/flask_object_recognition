22b481abeaceaf6407478aa382ba5f67ba4e2041
WisamAbbasi <39978331+WisamAbbasi@users.noreply.github.com>
2023-08-03 20:10:23 +0200

    Modify code Last

--- app.py
+++ app.py
@@ -31,188 +31,160 @@ def get_data():
     return analyzer_id
 
 
-def cap_analysis(frame_id, cap):
-    ret, frame = cap.read()
-    if not ret:
-        break
+@app.route(
+    "/cam_object/<cam_link>/<epsilon>/<sensitivity>/<requestor_id>/<requestor_type>/<request_id>"
+)
+def cam_object_recognition(
+    cam_link, epsilon, sensitivity, requestor_id, requestor_type, request_id
+):
+    analyzer_id = get_data()
 
-    img = np.asarray(frame)
+    # Get current date and time
+    now = datetime.datetime.now()
 
-    frame_id += 1
+    # Generate a random hash using SHA-256 algorithm
+    hash_object = hashlib.sha256()
+    hash_object.update(bytes(str(now), "utf-8"))
+    hash_value = hash_object.hexdigest()
 
-    # Generate Laplacian noise
-    noise = np.random.laplace(scale=scale_factor, size=img.shape)
+    # Concatenate the time and the hash
+    analysis_id = str(analyzer_id) + str(now) + hash_value
 
-    # Add the noise to the image
-    noisy_img = img + noise
+    # Load YOLOv3 network
+    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
 
-    noisy_img = cv2.convertScaleAbs(noisy_img)
-    frame = noisy_img
+    # Load COCO dataset class names
+    with open("coco.names", "r") as f:
+        classes = [line.strip() for line in f.readlines()]
 
-    # Convert the frame to a blob and pass it through the network
-    blob = cv2.dnn.blobFromImage(
-        frame, 1 / 255, (416, 416), swapRB=True, crop=False
-    )
-    net.setInput(blob)
-    outs = net.forward(net.getUnconnectedOutLayersNames())
+    # Generate random colors for each class
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+    labels = []
+    labels_dict = {}
 
-    return img, frame_id, noisy_img, frame, blob, outs
+    # Compute the scale factor for Laplacian noise
+    scale_factor = float(sensitivity) / float(epsilon)
 
+    # for object detection
+    cap = cv2.VideoCapture(cam_link)
 
-# @app.route(
-#     "/cam_object/<cam_link>/<epsilon>/<sensitivity>/<requestor_id>/<requestor_type>/<request_id>"
-# )
-# def cam_object_recognition(
-#     cam_link, epsilon, sensitivity, requestor_id, requestor_type, request_id
-# ):
-#     analyzer_id = get_data()
+    frame_id = 0
 
-#     # Get current date and time
-#     now = datetime.datetime.now()
+    while True:
+        # Read a frame from the video
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-#     # Generate a random hash using SHA-256 algorithm
-#     hash_object = hashlib.sha256()
-#     hash_object.update(bytes(str(now), "utf-8"))
-#     hash_value = hash_object.hexdigest()
+        img = np.asarray(frame)
 
-#     # Concatenate the time and the hash
-#     analysis_id = str(analyzer_id) + str(now) + hash_value
+        frame_id += 1
 
-#     # Load YOLOv3 network
-#     net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+        # Generate Laplacian noise
+        noise = np.random.laplace(scale=scale_factor, size=img.shape)
 
-#     # Load COCO dataset class names
-#     with open("coco.names", "r") as f:
-#         classes = [line.strip() for line in f.readlines()]
+        # Add the noise to the image
+        noisy_img = img + noise
 
-#     # Generate random colors for each class
-#     colors = np.random.uniform(0, 255, size=(len(classes), 3))
-#     labels = []
-#     labels_dict = {}
+        noisy_img = cv2.convertScaleAbs(noisy_img)
+        frame = noisy_img
 
-#     # Compute the scale factor for Laplacian noise
-#     scale_factor = float(sensitivity) / float(epsilon)
+        # Convert the frame to a blob and pass it through the network
+        blob = cv2.dnn.blobFromImage(
+            frame, 1 / 255, (416, 416), swapRB=True, crop=False
+        )
+        net.setInput(blob)
+        outs = net.forward(net.getUnconnectedOutLayersNames())
 
-#     # for object detection
-#     cap = cv2.VideoCapture(cam_link)
+        # Parse the network output and draw the detections on the frame
+        class_ids = []
+        confidences = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:
+                    center_x = int(detection[0] * frame.shape[1])
+                    center_y = int(detection[1] * frame.shape[0])
+                    w = int(detection[2] * frame.shape[1])
+                    h = int(detection[3] * frame.shape[0])
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+                    class_ids.append(class_id)
+                    confidences.append(float(confidence))
+                    boxes.append([x, y, w, h])
 
-#     frame_id = 0
+        # Apply non-maximum suppression to remove overlapping detections
+        indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
 
-#     while True:
-#         # Read a frame from the video
-#         ret, frame = cap.read()
-#         if not ret:
-#             break
+        # Draw the detections on the frame
+        for i in indices:
+            i = i[0]
+            x, y, w, h = boxes[i]
+            label = classes[class_ids[i]]
+            labels.append(label)
+            color = colors[class_ids[i]]
+            # cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
+            # cv2.putText(frame, label, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-#         img = np.asarray(frame)
+        print(labels)
 
-#         frame_id += 1
+        ws_req = {
+            "RequestPostTopicUUID": {
+                "topic_name": "SIFIS:Object_Recognition_Frame_Results",
+                "topic_uuid": "Object_Recognition_Frame_Results",
+                "value": {
+                    "description": "Object Recognition Frame Results",
+                    "requestor_id": str(requestor_id),
+                    "requestor_type": str(requestor_type),
+                    "analyzer_id": str(analyzer_id),
+                    "analysis_id": str(analysis_id),
+                    "Type": "CAM",
+                    "file_name": "Empty",
+                    "epsilon": float(epsilon),
+                    "sensitivity": float(sensitivity),
+                    "scale_factor": float(scale_factor),
+                    "frame_id": int(frame_id),
+                    "labels": labels,
+                },
+            }
+        }
+        ws.send(json.dumps(ws_req))
 
-#         # Generate Laplacian noise
-#         noise = np.random.laplace(scale=scale_factor, size=img.shape)
+        labels_dict[frame_id] = labels
+        labels = []
+        if cv2.waitKey(1) == ord("q"):
+            break
 
-#         # Add the noise to the image
-#         noisy_img = img + noise
+    # Release the video file and close all windows
+    cap.release()
+    cv2.destroyAllWindows()
 
-#         noisy_img = cv2.convertScaleAbs(noisy_img)
-#         frame = noisy_img
+    ws_req_final = {
+        "RequestPostTopicUUID": {
+            "topic_name": "SIFIS:Object_Recognition_Results",
+            "topic_uuid": "Object_Recognition_Results",
+            "value": {
+                "description": "Object Recognition Results",
+                "requestor_id": str(requestor_id),
+                "requestor_type": str(requestor_type),
+                "request_id": str(request_id),
+                "analyzer_id": str(analyzer_id),
+                "analysis_id": str(analysis_id),
+                "Type": "CAM",
+                "file_name": "Empty",
+                "epsilon": float(epsilon),
+                "sensitivity": float(sensitivity),
+                "scale_factor": float(scale_factor),
+                "labels dictionary": labels_dict,
+            },
+        }
+    }
 
-#         # Convert the frame to a blob and pass it through the network
-#         blob = cv2.dnn.blobFromImage(
-#             frame, 1 / 255, (416, 416), swapRB=True, crop=False
-#         )
-#         net.setInput(blob)
-#         outs = net.forward(net.getUnconnectedOutLayersNames())
-
-#         # Parse the network output and draw the detections on the frame
-#         class_ids = []
-#         confidences = []
-#         boxes = []
-#         for out in outs:
-#             for detection in out:
-#                 scores = detection[5:]
-#                 class_id = np.argmax(scores)
-#                 confidence = scores[class_id]
-#                 if confidence > 0.5:
-#                     center_x = int(detection[0] * frame.shape[1])
-#                     center_y = int(detection[1] * frame.shape[0])
-#                     w = int(detection[2] * frame.shape[1])
-#                     h = int(detection[3] * frame.shape[0])
-#                     x = int(center_x - w / 2)
-#                     y = int(center_y - h / 2)
-#                     class_ids.append(class_id)
-#                     confidences.append(float(confidence))
-#                     boxes.append([x, y, w, h])
-
-#         # Apply non-maximum suppression to remove overlapping detections
-#         indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-
-#         # Draw the detections on the frame
-#         for i in indices:
-#             i = i[0]
-#             x, y, w, h = boxes[i]
-#             label = classes[class_ids[i]]
-#             labels.append(label)
-#             color = colors[class_ids[i]]
-#             # cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
-#             # cv2.putText(frame, label, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-#         print(labels)
-
-#         ws_req = {
-#             "RequestPostTopicUUID": {
-#                 "topic_name": "SIFIS:Object_Recognition_Frame_Results",
-#                 "topic_uuid": "Object_Recognition_Frame_Results",
-#                 "value": {
-#                     "description": "Object Recognition Frame Results",
-#                     "requestor_id": str(requestor_id),
-#                     "requestor_type": str(requestor_type),
-#                     "analyzer_id": str(analyzer_id),
-#                     "analysis_id": str(analysis_id),
-#                     "Type": "CAM",
-#                     "file_name": "Empty",
-#                     "epsilon": float(epsilon),
-#                     "sensitivity": float(sensitivity),
-#                     "scale_factor": float(scale_factor),
-#                     "frame_id": int(frame_id),
-#                     "labels": labels,
-#                 },
-#             }
-#         }
-#         ws.send(json.dumps(ws_req))
-
-#         labels_dict[frame_id] = labels
-#         labels = []
-#         if cv2.waitKey(1) == ord("q"):
-#             break
-
-#     # Release the video file and close all windows
-#     cap.release()
-#     cv2.destroyAllWindows()
-
-#     ws_req_final = {
-#         "RequestPostTopicUUID": {
-#             "topic_name": "SIFIS:Object_Recognition_Results",
-#             "topic_uuid": "Object_Recognition_Results",
-#             "value": {
-#                 "description": "Object Recognition Results",
-#                 "requestor_id": str(requestor_id),
-#                 "requestor_type": str(requestor_type),
-#                 "request_id": str(request_id),
-#                 "analyzer_id": str(analyzer_id),
-#                 "analysis_id": str(analysis_id),
-#                 "Type": "CAM",
-#                 "file_name": "Empty",
-#                 "epsilon": float(epsilon),
-#                 "sensitivity": float(sensitivity),
-#                 "scale_factor": float(scale_factor),
-#                 "labels dictionary": labels_dict,
-#             },
-#         }
-#     }
-
-#     ws.send(json.dumps(ws_req_final))
-#     return ws_req_final
+    ws.send(json.dumps(ws_req_final))
+    return ws_req_final
 
 
 @app.route(
@@ -271,9 +243,30 @@ def file_object_recognition(
         frame_id = 0
 
         while True:
-            img, frame_id, noisy_img, frame, blob, outs = cap_analysis(
-                frame_id, cap
+            # Read a frame from the video
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            img = np.asarray(frame)
+
+            frame_id += 1
+
+            # Generate Laplacian noise
+            noise = np.random.laplace(scale=scale_factor, size=img.shape)
+
+            # Add the noise to the image
+            noisy_img = img + noise
+
+            noisy_img = cv2.convertScaleAbs(noisy_img)
+            frame = noisy_img
+
+            # Convert the frame to a blob and pass it through the network
+            blob = cv2.dnn.blobFromImage(
+                frame, 1 / 255, (416, 416), swapRB=True, crop=False
             )
+            net.setInput(blob)
+            outs = net.forward(net.getUnconnectedOutLayersNames())
 
             # Parse the network output and draw the detections on the frame
             class_ids = []
