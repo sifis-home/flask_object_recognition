c0fad76f4bc881cf94db6f4d29975268c254b142
WisamAbbasi <39978331+WisamAbbasi@users.noreply.github.com>
2023-08-14 16:52:52 +0200

    Change shape

--- test_flask_object_recognition.py
+++ test_flask_object_recognition.py
@@ -124,7 +124,7 @@ def test_add_noise():
     items = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     sensitivity = 1
     epsilon = 0.3
-    img_size = items.shape
+    img_size = np.asarray(items).shape
 
     scale_factor = get_scale(sensitivity, epsilon)
     expected_noise = np.random.laplace(scale=scale_factor, size=img_size)
